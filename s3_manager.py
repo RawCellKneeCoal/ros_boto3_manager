751765e710e4678735656dac70ff3ed6691cece5
EC2 Default User <ec2-user@ip-172-31-28-139.ap-southeast-1.compute.internal>
2022-07-20 06:21:13 +0000

    Renamed 1_s3_example.py to s3_manager.py

---
+++ s3_manager.py
@@ -0,0 +1,15 @@
+# ROSELE
+
+import logging
+import random
+import uuid
+from datetime import datetime
+from decimal import Decimal
+from pathlib import Path, PosixPath
+
+import boto3
+from botocore.exceptions import ClientError
+
+logging.basicConfig(
+level=logging.INFO, format='[%(asctime)s] %(levelname)s %(module)s %(lineno)d - %(message)s')
+log = logging.getLogger()
