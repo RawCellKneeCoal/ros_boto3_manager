d99686faecf8fb6404549b44336f1a22a377c3e8
EC2 Default User <ec2-user@ip-172-31-28-139.ap-southeast-1.compute.internal>
2022-07-22 14:13:59 +0000

    Finished cwlogs_manager

--- cwlogs_manager.py
+++ cwlogs_manager.py
@@ -14,3 +14,41 @@ logging.basicConfig(
 level=logging.INFO, format='[%(asctime)s] %(levelname)s %(module)s %(lineno)d - %(message)s',)
 log = logging.getLogger()
 
+
+# List log groups and log streams
+def list_log_groups(group_name=None, region_name=None):
+    cwlogs = boto3.client('logs', region_name=region_name)
+    params = {
+        'logGroupNamePrefix': group_name,
+    } if group_name else {}
+    res = cwlogs.describe_log_groups(**params)
+    return res['logGroups']
+
+def list_log_group_streams(group_name, stream_name=None, region_name=None):
+    cwlogs = boto3.client('logs', region_name=region_name)
+    params = {
+        'logGroupName': group_name,
+    } if group_name else {}
+    if stream_name:
+        params['logStreamNamePrefix'] = stream_name
+    res = cwlogs.describe_log_streams(**params)
+    return res['logStreams']
+
+# Filter log events
+def filter_log_events(
+    group_name, filter_pat,
+    start=None, stop=None,
+    region_name=None
+):
+    cwlogs = boto3.client('logs', region_name=region_name)
+    params = {
+        'logGroupName': group_name,
+        'filterPattern': filter_pat,
+    }
+    if start:
+        params['startTime'] = start
+    if stop:
+        params['endTime'] = stop
+    res = cwlogs.filter_log_events(**params)
+    return res['events']
+
